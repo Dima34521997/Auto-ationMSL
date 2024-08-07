b7ef3d6310524a0233d3d42dd2f3de80ebd1c68a
Dmitry <dima1997mail@gmail.com>
2024-08-07 20:40:35 +0700

    работает с 3 таблицами

---
+++ main.py
@@ -0,0 +1,14 @@
+import locale
+import sys
+import time
+
+from PySide6 import QtWidgets
+from Window import Window
+
+locale.setlocale(locale.LC_ALL, ('ru_RU', 'UTF-8'))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
